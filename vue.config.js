e87739a5816a752593364ae7aa8f1acdc1424bb0
Alexey B. Podyapolsky <alpody@yandex.ru>
2019-05-29 15:08:52 +0300

    Add settings for dev server

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  lintOnSave: false,
-  productionSourceMap: false
+    lintOnSave: false,
+    productionSourceMap: false,
+    devServer: {
+                disableHostCheck: false,
+                port: 8080,
+                public: '0.0.0.0:8080'
+            }
 }
